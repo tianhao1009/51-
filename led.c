34e4ba17f20a8d3502d81f5ba1e3c47ce9c8479b
tianhao1009 <95536052+tianhao1009@users.noreply.github.com>
2022-01-13 16:34:02 +0800

    Add files via upload

---
+++ led.c
@@ -0,0 +1,17 @@
+#include <reg52.h>
+sbit ENLRD = P1^4;
+sbit LED1 = P0^0;
+sbit LED2 = P0^1;
+sbit LED3 = P0^2;
+sbit LED4 = P0^3;
+sbit LED5 = P0^4;
+sbit LED6 = P0^5;
+sbit LED7 = P0^6;
+sbit LED8 = P0^7;
+void main()
+{
+	while(1)
+	switch(i)
+		case 1 :{ for(;i=1;)
+	for(t=0;t<1000
+}
